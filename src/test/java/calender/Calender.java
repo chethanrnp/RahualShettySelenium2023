8be8cab1d7d7346421f1bf2a9a6109b07386dd4e
chethanrnp <chethan.supernova@gmail.com>
2023-08-26 06:29:52 +0530

    Nothing

--- src/test/java/calender/Calender.java
+++ src/test/java/calender/Calender.java
@@ -14,7 +14,7 @@ public class Calender {
 
 	@Test
 	public void calender() {
-		System.setProperty("webdriver.chrome.driver", ".\\src\\main\\resources\\chromedriver_2.exe");
+		System.setProperty("webdriver.chrome.driver", ".\\src\\main\\resources\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(options);
